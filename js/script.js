e292e098b5da7e174b0dbd954a7d926c2ca05c5a
Matt Hsiung <xung@musician.com>
2016-05-06 09:45:06 -0400

    done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
- //    var controller = new ScrollMagic.Controller({
- //    	addIndicators: true,
- //    	container: '.wrap'
- //    });
+	var height= $('.bg').height()
+  	$( window ).resize(function() {
+  		height= $('.bg').height()
+  	});
 
-	// var scene = new ScrollMagic.Scene({
- //        triggerElement: '#team',
- //    })
- //    .setClassToggle('.hidden', 'show')
- //    // .on('start', function(){controller.scrollTo('#contact')})
- //    .addTo(controller);
+ 	$('.wrap').scroll(function(){
+		var pos = $(this).scrollTop();
+		if(pos>5&&pos<height/2){
+			$('#wingl').css({
+				transform: 'translateY('+pos+'px) rotate(-45deg)',
+				opacity: 1 - (pos/height)*3
+			});
+		}
+		if(pos>height/2){
+			$('#wingl').css({
+				transform: 'translateY('+(height-pos)+'px) rotate(-45deg)',
+				opacity: (pos/height)*3
+			});
+			
+		}
+ 	});
+
  	$('#nav').click(function(){
  		$(this).toggleClass('close')
- 	})
+ 	});
+
     var $people = $('#people');
     $('#right').click(right);
     $('#left').click(left);
@@ -20,12 +32,10 @@ $(document).ready(function() {
     function right(){
     	(start < $('.person').length-1)? ++start : start=0
     	$people.css({transform: 'translateX('+ -100*start +'vw)'})
-    	console.log(start, $('.person'));
     }
     function left(){
     	(start ===0)? start= $('.person').length-1 : --start
     	$people.css({transform: 'translateX('+ -100*start +'vw)'})
-    	console.log(start, $('.person'));
     }
 
 });
